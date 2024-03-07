fdf9447bba506aeab75e8bb9faa4140496c1db2f
sandyrop2 <130761216+sandyrop2@users.noreply.github.com>
2024-03-07 15:28:53 +0530

    Add files via upload

---
+++ new.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+main()
+{
+int m;
+while(1){
+printf("enter the number\n");
+scanf("%d",&m);
+if(m%2==0)
+printf("the number is even\n");
+else
+    printf("the number is odd\n");
+}
+
+}
