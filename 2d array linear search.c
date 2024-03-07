4b1d6b2ec491dd0f8bff24fc40cacd9df53a8447
sandyrop2 <130761216+sandyrop2@users.noreply.github.com>
2024-03-07 15:27:31 +0530

    Add files via upload

---
+++ 2d array linear search.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+void read(int a[100][100],int,int);
+void dis(int a[100][100],int,int);
+void dup(int a[100][100],int,int);
+main()
+{
+    int r,c,a[100][100];
+    printf("enter the number of rows and column\n");
+    scanf("%d%d",&r,&c);
+    read(a,r,c);
+    dis(a,r,c);
+    dup(a,r,c);
+
+}
+void read(int a[100][100],int r,int c)
+{
+    int i,j;
+    printf("enter the values\n");
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<c;j++)
+            scanf("%d",&a[i][j]);
+    }
+}
+void dis(int a[100][100],int r,int c)
+{
+    int i,j;
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<c;j++)
+            printf("%d ",a[i][j]);
+
+    printf("\n");}
+}
+void dup(int a[100][100],int r,int c)
+{
+   int i,j,s=0;
+   for(i=0;i<r;i++)
+   {
+       s=0;
+       for(j=0;j<c;j++)
+       {
+           s=s+a[i][j];
+       }
+       printf("sum is %d",s);
+   }
+}
