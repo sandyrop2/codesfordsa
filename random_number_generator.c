a50def42e55cd663702c5a5adf4df94955374ee9
sandyrop2 <130761216+sandyrop2@users.noreply.github.com>
2024-03-13 05:16:21 +0530

    Add files via upload

---
+++ random_number_generator.c
@@ -0,0 +1,30 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+void printRandoms(int lower, int upper,
+							int count)
+{
+	int i;
+	for (i = 0; i < count; i++) {
+		int num = (rand() %
+		(upper - lower + 1)) + lower;
+		printf("%d ", num);
+	}
+}
+
+
+int main()
+{
+	int lower = 5, upper = 7, count = 1;
+
+
+	srand(time(0));
+
+	printRandoms(lower, upper, count);
+
+	return 0;
+}
+
