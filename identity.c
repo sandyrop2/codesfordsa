a5fc0d99b343b2660b2ec702203d479bbb4726a1
sandyrop2 <130761216+sandyrop2@users.noreply.github.com>
2024-03-13 21:57:05 +0530

    Add files via upload

---
+++ identity.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+int main() {
+    int arr1[10][10];
+    int r1, c1;
+    int i, j, yn = 1;
+   
+    printf("Input number of Rows for the matrix :");
+    scanf("%d", &r1);
+   
+    printf("Input number of Columns for the matrix :");
+    scanf("%d", &c1);
+	
+    
+    printf("Input elements in the matrix :\n");
+    for(i = 0; i < r1; i++) {
+        for(j = 0; j < c1; j++) {
+            printf("element - [%d],[%d] : ", i, j);
+            scanf("%d", &arr1[i][j]);
+        }
+    }
+    
+    printf("The matrix is :\n");
+    for(i = 0; i < r1; i++) {
+        for(j = 0; j < c1 ;j++) {
+            printf("% 4d", arr1[i][j]);
+        }
+        printf("\n");
+    }
+    
+    for(i = 0; i < r1; i++) {
+        for(j = 0; j < c1; j++) {
+            if ((i == j && arr1[i][j] != 1) || (i != j && arr1[i][j] != 0)) {
+                yn = 0;
+                break;
+            }
+        }
+        if (yn == 0) {
+            break;
+        }
+    }
+	
+    if(yn == 1) {
+        printf(" The matrix is an identity matrix.\n\n");
+    } else {
+        printf(" The matrix is not an identity matrix.\n\n");
+    }
+    return 0;
+}
